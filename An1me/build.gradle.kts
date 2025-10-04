95691339ba0d0c427c05224a11b66a56d04b24bb
Lliakos <philip_k06@yahoo.com>
2025-10-04 18:50:31 +0300

    Pls no error

--- An1me/build.gradle.kts
+++ An1me/build.gradle.kts
@@ -7,9 +7,6 @@ cloudstream {
     status = 1
     tvTypes = listOf("Anime", "AnimeMovie", "OVA")
     iconUrl = "https://an1me.to/favicon.ico"
-    
-    // Add this to control the output name
-    name = "An1me"
 }
 
 android {
