ce3763d36fb882c021b41f83ad97b995e9462664
Lliakos <philip_k06@yahoo.com>
2025-10-11 12:22:41 +0300

    Use newExtractorLink function instead of deprecated constructor

--- An1me/src/main/kotlin/com/an1me/An1meProvider.kt
+++ An1me/src/main/kotlin/com/an1me/An1meProvider.kt
@@ -134,13 +134,12 @@ class An1meProvider : MainAPI() {
                     
                     // Add the M3U8 URL directly - let the player handle it
                     callback.invoke(
-                        ExtractorLink(
-                            name,
-                            name,
-                            videoUrl,
-                            mainUrl,
-                            Qualities.Unknown.value,
-                            true // isM3u8
+                        newExtractorLink(
+                            name = name,
+                            url = videoUrl,
+                            refererUrl = mainUrl,
+                            quality = Qualities.Unknown.value,
+                            isM3u8 = true
                         )
                     )
                     android.util.Log.d("An1me_Video", "Added M3U8 link successfully")
